64377302d8d04a66ed7b4def787e7a1c5d68f4b2
Jean HT <Alexandro.torres1232@gmail.com>
2025-12-19 17:05:34 -0500

    Primer commit

--- views/productos/listar.php
+++ views/productos/listar.php
@@ -4,8 +4,30 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Productos</title>
+  <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
 </head>
 <body>
-  <h1>Listar productos activos</h1>
+  <div class="container">
+    <h1>Lista de productos</h1>
+    <table class="table table-striped">
+      <thead>
+        <tr></tr>
+          <th>ID</th>
+          <th>Clasificación</th>
+          <th>Marca</th>
+          <th>Descripción</th>
+          <th>Garantía</th>
+          <th>Ingreso</th>
+          <th>Cantidad</th>
+          <th>Operaciones</th>
+        </tr>
+
+      </thead>
+      <tbody>
+        <!-- Contenido dinamico, viene desde la BD -->
+      </tbody>
+    </table>
+  </div>
+
 </body>
 </html>
